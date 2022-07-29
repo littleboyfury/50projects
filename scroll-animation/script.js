53ac78aee5aa71165ee57f3dc375216a318a36e8
skyfury <littleboyfuyry@gmail.com>
2022-07-29 10:45:19 +0800

    feat: add scroll animation

--- scroll-animation/script.js
+++ scroll-animation/script.js
@@ -1,13 +1,16 @@
 const boxes = document.querySelectorAll('.box')
 
+// 添加滚动监听器
 window.addEventListener('scroll', checkBoxes)
 
 checkBoxes()
 
 function checkBoxes() {
+    // window 高度，视口高度
     const triggerBottom = window.innerHeight / 5 * 4
 
     boxes.forEach(box => {
+        // 距离视口顶部的距离
         const boxTop = box.getBoundingClientRect().top
 
         if(boxTop < triggerBottom) {
