2a4c3871b11170151abce0681c49faeb06799734
skyfury <littleboyfuyry@gmail.com>
2022-08-02 18:36:09 +0800

    feat: add choice picker

--- random-choice-picker/script.js
+++ random-choice-picker/script.js
@@ -4,66 +4,73 @@ const textarea = document.getElementById('textarea')
 textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
-    createTags(e.target.value)
+  createTags(e.target.value)
 
-    if(e.key === 'Enter') {
-        setTimeout(() => {
-            e.target.value = ''
-        }, 10)
+  if (e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = ''
+    }, 10)
 
-        randomSelect()
-    }
+    randomSelect()
+  }
 })
 
 function createTags(input) {
-    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
-    
-    tagsEl.innerHTML = ''
+  const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
 
-    tags.forEach(tag => {
-        const tagEl = document.createElement('span')
-        tagEl.classList.add('tag')
-        tagEl.innerText = tag
-        tagsEl.appendChild(tagEl)
-    })
+  tagsEl.innerHTML = ''
+
+  tags.forEach(tag => {
+    const tagEl = document.createElement('span')
+    tagEl.classList.add('tag')
+    tagEl.innerText = tag
+    tagsEl.appendChild(tagEl)
+  })
 }
 
 function randomSelect() {
-    const times = 30
+  const times = 30
 
-    const interval = setInterval(() => {
-        const randomTag = pickRandomTag()
-	
-	if (randomTag !== undefined) {
-        highlightTag(randomTag)
+  // 无线循环，间隔 100 ms
+  const interval = setInterval(() => {
+    // 随机选择一个 tag
+    const randomTag = pickRandomTag()
 
-        setTimeout(() => {
-            unHighlightTag(randomTag)
-        }, 100)
-	}
-    }, 100);
+    if (randomTag !== undefined) {
+      // 高亮 tag
+      highlightTag(randomTag)
+
+      setTimeout(() => {
+        // 定时 100 ms 清理高亮
+        unHighlightTag(randomTag)
+      }, 100)
+    }
+  }, 100);
+
+  setTimeout(() => {
+    // 清理循环器
+    clearInterval(interval)
 
     setTimeout(() => {
-        clearInterval(interval)
+      // 随机选一个 tag
+      const randomTag = pickRandomTag()
 
-        setTimeout(() => {
-            const randomTag = pickRandomTag()
+      // 设置高亮
+      highlightTag(randomTag)
+    }, 100)
 
-            highlightTag(randomTag)
-        }, 100)
-
-    }, times * 100)
+  }, times * 100)
 }
 
 function pickRandomTag() {
-    const tags = document.querySelectorAll('.tag')
-    return tags[Math.floor(Math.random() * tags.length)]
+  const tags = document.querySelectorAll('.tag')
+  return tags[Math.floor(Math.random() * tags.length)]
 }
 
 function highlightTag(tag) {
-    tag.classList.add('highlight')
+  tag.classList.add('highlight')
 }
 
 function unHighlightTag(tag) {
-    tag.classList.remove('highlight')
+  tag.classList.remove('highlight')
 }
