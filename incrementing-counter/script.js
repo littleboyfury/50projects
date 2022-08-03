18258193bbb4459d789da53c1f7fc8ec944709a8
skyfury <littleboyfuyry@gmail.com>
2022-08-03 17:07:44 +0800

    feat: add incrementing counter

--- incrementing-counter/script.js
+++ incrementing-counter/script.js
@@ -4,6 +4,7 @@ counters.forEach(counter => {
     counter.innerText = '0'
 
     const updateCounter = () => {
+        // 获取自定义属性，+ 表示转为数字类型
         const target = +counter.getAttribute('data-target')
         const c = +counter.innerText
 
@@ -11,6 +12,8 @@ counters.forEach(counter => {
 
         if(c < target) {
             counter.innerText = `${Math.ceil(c + increment)}`
+
+            // 递归定时增加数字
             setTimeout(updateCounter, 1)
         } else {
             counter.innerText = target
